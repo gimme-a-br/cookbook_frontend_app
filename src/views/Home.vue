1b400116e4415e2aafdf7bc691dce2257125ff5f
Peter Jang <peter.x.jang@gmail.com>
2020-10-12 19:40:08 -0500

    Add recipes index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>All recipes</h1>
+    <div v-for="recipe in recipes">
+      <h2>Title: {{ recipe.title }}</h2>
+      <p>Chef: {{ recipe.chef }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Home",
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      recipes: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexRecipes();
+  },
+  methods: {
+    indexRecipes: function () {
+      axios.get("/api/recipes").then((response) => {
+        console.log("All recipes:", response.data);
+        this.recipes = response.data;
+      });
+    },
+  },
 };
 </script>
