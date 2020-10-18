66c835e2c3aa83d3aec21d0eecb3fb3f7b2427c3
Peter Jang <peter.x.jang@gmail.com>
2020-10-18 10:58:01 -0500

    Add recipes index page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Test from "../views/Test.vue";
 import Signup from "../views/Signup.vue";
 import Login from "../views/Login.vue";
 import Logout from "../views/Logout.vue";
+import RecipesIndex from "../views/RecipesIndex.vue";
 
 Vue.use(VueRouter);
 
@@ -26,6 +27,7 @@ const routes = [
   { path: "/signup", name: "signup", component: Signup },
   { path: "/login", name: "login", component: Login },
   { path: "/logout", name: "logout", component: Logout },
+  { path: "/recipes", name: "recipes-index", component: RecipesIndex },
 ];
 
 const router = new VueRouter({
---
+++ src/views/RecipesIndex.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="recipes-index">
+    <h1>All recipes</h1>
+    <div v-for="recipe in recipes">
+      <h2>Title: {{ recipe.title }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "RecipesIndex",
+  data: function () {
+    return {
+      recipes: [],
+    };
+  },
+  created: function () {
+    this.indexRecipes();
+  },
+  methods: {
+    indexRecipes: function () {
+      axios.get("/api/recipes").then((response) => {
+        console.log("All recipes:", response.data);
+        this.recipes = response.data;
+      });
+    },
+  },
+};
+</script>
